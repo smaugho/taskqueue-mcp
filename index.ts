da1ae977a8dd5e06af50ebd7ce8d0f006e496086
Christopher C. Smith <chriscarrollsmith@gmail.com>
2025-03-23 16:51:52 -0400

    Version bump

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ import { ListToolsRequestSchema, CallToolRequestSchema } from "@modelcontextprot
 const server = new Server(
   {
     name: "task-manager-server",
-    version: "1.0.7"
+    version: "1.0.8"
   },
   {
     capabilities: {
